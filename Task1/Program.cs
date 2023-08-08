404d147c319f9ccbb7f3dcfb1df623f3a01359d4
Aleksandr <sosnin93@yandex.ru>
2023-08-08 13:49:52 +0300

    Second

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Введите трехзначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+int firstdDigit = number / 100;
+int seconddDigit = number / 10 % 10;
+
+Console.WriteLine($"Вторая цифра в числе {number} равна {seconddDigit}");
