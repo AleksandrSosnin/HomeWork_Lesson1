e30d686abff8751ee482f9ea3b35e14ec71f17f4
Aleksandr <sosnin93@yandex.ru>
2023-08-08 14:49:18 +0300

    3

---
+++ Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+
+int dayNumber = ReadInt("Введите число от 1 до 7: , не вводить число, больше 7! ");
+Console.WriteLine(WorkHoliday(dayNumber));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+string WorkHoliday(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("Под цифрой " + a + " - Выходной");
+        }
+        else
+        {
+            Console.Write("Под цифрой " + a + " - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Я же просил, не вводить, ");
+    }
+    return " ошибка.";
+}
